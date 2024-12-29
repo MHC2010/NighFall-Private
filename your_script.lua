5a1f69cbe4fffecbe7b756b73e6ae31a7cd30ccf
MHC2010 <137643972+MHC2010@users.noreply.github.com>
2024-12-29 08:58:50 -0700

    your_script.lua

--- your_script.lua
+++ your_script.lua
@@ -1,4 +1,5 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusMenu/rayfield/main/Source'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield
+'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "NightFall Hub",
